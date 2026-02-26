03f6561fcad5a6ccb3435fc547af4fec45bfb254
OnlYKai <67548894+OnlYKai@users.noreply.github.com>
2026-02-26 21:28:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const api_key = process.env.API_KEY
 const webhook_url = process.env.WEBHOOK_URL
 
 /// SETTINGS START ///
-const mature = true
-const country = 'DE' // https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
-const shops = [61, 16] // https://api.isthereanydeal.com/service/shops/v1
-const role_id = '1475596100160323768'
+const include_mature = true
+const country_code = 'DE' // https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+const shop_ids = [61, 16] // https://api.isthereanydeal.com/service/shops/v1
 const message = (game) => {
+    const role_id = '1475596100160323768'
     const timestamp = new Date(game.deal.expiry).getTime() / 1000
     const regular_price = `${game.deal.regular.amount} ${game.deal.regular.currency}`
     const current_price = game.deal.price.amount === 0 ? 'FREE' : `${game.deal.price.amount } ${game.deal.price.currency}`
@@ -33,7 +33,7 @@ const message = (game) => {
 
 const filter = 'N4IgDgTglgxgpiAXKAtlAdk9BXANrgGhBQEMAPJABgF9qg%3D%3D' // Max Price = 0; Way faster if used with sort=price, probably because it stops checking after encountering the first too expensive game.
 
-const url = `https://api.isthereanydeal.com/deals/v2?key=${api_key}&limit=200&sort=price&filter=${filter}&mature=${mature}&country=${country}&shops=${shops.join()}`
+const url = `https://api.isthereanydeal.com/deals/v2?key=${api_key}&limit=200&sort=price&filter=${filter}&mature=${include_mature}&country=${country_code}&shops=${shop_ids.join()}`
 
 const deals_response = await fetch(url)
 const deals = await deals_response.json()
